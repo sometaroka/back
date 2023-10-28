7a0b28a3dc261b26cfcf2601bae511e7e1255c55
273* <keita.y310@icloud.com>
2023-10-28 15:51:58 +0900

    POST_methodに対応しました．

--- src/sometaroka/models.py
+++ src/sometaroka/models.py
@@ -3,132 +3,145 @@ from django.db import models
 # Create your models here.
 
 
-#ここから記述しました。もうちょっと余裕もって依頼して欲しかったな。朝にバイトあるし、明日イベント運営しなきゃいけないんだよ？
-#正直私って結構忙しい部類の人間だと思うんだ、そんな私にばっか何でもかんでもやってもらいすぎだともうし、こんな構造になっているのは
-#絶対おかしいと思うんだよ。いかがでしょうか　←　ごめんなさい
+# ここから記述しました。もうちょっと余裕もって依頼して欲しかったな。朝にバイトあるし、明日イベント運営しなきゃいけないんだよ？
+# 正直私って結構忙しい部類の人間だと思うんだ、そんな私にばっか何でもかんでもやってもらいすぎだともうし、こんな構造になっているのは
+# 絶対おかしいと思うんだよ。いかがでしょうか　←　ごめんなさい
 from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
 import uuid
 
 
-#都道府県テーブル
+# 都道府県テーブル
 class PrefecturesModel(models.Model):
-    #都道府県ID
-    prefectures_id = models.SmallIntegerField(primary_key=True,)
-    #都道府県名
+    # 都道府県ID
+    prefectures_id = models.SmallIntegerField(
+        primary_key=True,
+    )
+    # 都道府県名
     prefectures_name = models.CharField(max_length=10)
 
-#イベントテーブル
-class EventsModel(models.Model):
-    #イベント情報ID
-    event_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    #イベント情報
-    event_info = models.CharField(max_length=400)
-    #都道府県ID
-    prefectures_id = models.ForeignKey(PrefecturesModel,on_delete=models.CASCADE,null=True)
 
-#方言テーブル
+# イベントテーブル
+class EventsModel(models.Model):
+    # イベント情報ID
+    event_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    # イベント情報
+    event_info = models.CharField(max_length=400)
+    # 都道府県ID
+    prefectures_id = models.ForeignKey(
+        PrefecturesModel, on_delete=models.CASCADE, null=True
+    )
+
+
+# 方言テーブル
 class DialectsModel(models.Model):
-    #方言ID
+    # 方言ID
     dialect_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    #方言名
+    # 方言名
     dialect_name = models.CharField(max_length=50)
-    
-#ユーザーテーブル
+
+
+# ユーザーテーブル
 class Users(AbstractUser):
-    #ユーザーネーム
-    #username = models.CharField(max_length=30,unique=True)
-    #パスワード
-    #password = models.CharField(max_length=16)
-    #テーブル設計ではバーキャラだけど修正した
-    #メールアドレス
-    #email = models.EmailField(max_length=319)
-    #プロフィールの自己紹介文
+    # ユーザーネーム
+    # username = models.CharField(max_length=30,unique=True)
+    # パスワード
+    # password = models.CharField(max_length=16)
+    # テーブル設計ではバーキャラだけど修正した
+    # メールアドレス
+    # email = models.EmailField(max_length=319)
+    # プロフィールの自己紹介文
     profile_introduction = models.CharField(max_length=300)
-    #出身地
+    # 出身地
     birth_place = models.CharField(max_length=10)
-    
+
     # オンライン状態
     statement = models.BooleanField(default=True)
-    #生年月日
+    # 生年月日
     date_of_birth = models.CharField(max_length=10)
-    #プロフィールのヘッダー画像
+    # プロフィールのヘッダー画像
     profile_header_image = models.CharField(max_length=100)
-    #プロフィールのアイコン画像
+    # プロフィールのアイコン画像
     profile_icon_image = models.CharField(max_length=100)
-    #方言ID
-    dialect_id = models.ForeignKey(DialectsModel,on_delete=models.CASCADE,null=True)
+    # 方言ID
+    dialect_id = models.ForeignKey(DialectsModel, on_delete=models.CASCADE, null=True)
 
-#お気に入り方言テーブル
+
+# お気に入り方言テーブル
 class FavoritesModels(models.Model):
-    #お気に入り方言ID
+    # お気に入り方言ID
     favorite_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    #方言ID
-    dialect_id = models.ForeignKey(DialectsModel,on_delete=models.CASCADE,null=True)
-    #ユーザーID
-    user = models.ForeignKey(Users,on_delete=models.CASCADE,null=True)
+    # 方言ID
+    dialect_id = models.ForeignKey(DialectsModel, on_delete=models.CASCADE, null=True)
+    # ユーザーID
+    user = models.ForeignKey(Users, on_delete=models.CASCADE, null=True)
 
-#タイムラインテーブル
+
+# タイムラインテーブル
 class TimelinesModel(models.Model):
-    #投稿ID
+    # 投稿ID
     post_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    #投稿画像
+    # 投稿画像
     post_image = models.CharField(max_length=100)
-    #投稿ボイス
+    # 投稿ボイス
     post_voice = models.CharField(max_length=100)
-    #投稿動画
+    # 投稿動画
     post_movie = models.CharField(max_length=100)
-    #いいね
+    # いいね
     good = models.IntegerField()
-    #投稿日時
+    # 投稿日時
     post_date = models.DateTimeField(auto_now_add=True)
-    #ユーザーID
-    user = models.ForeignKey(Users,on_delete=models.CASCADE,null=True)
+    # ユーザーID
+    user = models.ForeignKey(Users, on_delete=models.CASCADE, null=True)
 
-#コメントテーブル
+
+# コメントテーブル
 class CommentsModel(models.Model):
-    #コメントID
+    # コメントID
     comment_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    #コメントデータ
+    # コメントデータ
     comment = models.CharField(max_length=100)
-    #投稿ID
-    post = models.ForeignKey(TimelinesModel,on_delete=models.CASCADE,null=True)
-    #ユーザーID
-    user = models.ForeignKey(Users,on_delete=models.CASCADE,null=True)
+    # 投稿ID
+    post = models.ForeignKey(TimelinesModel, on_delete=models.CASCADE, null=True)
+    # ユーザーID
+    user = models.ForeignKey(Users, on_delete=models.CASCADE, null=True)
 
-#いいねテーブル
+
+# いいねテーブル
 class LikesModel(models.Model):
-    #いいね数
+    # いいね数
     like_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    #ユーザーID
-    user = models.ForeignKey(Users,on_delete=models.CASCADE,null=True)
+    # ユーザーID
+    user = models.ForeignKey(Users, on_delete=models.CASCADE, null=True)
 
-#フォロー/フォロワーテーブル
+
+# フォロー/フォロワーテーブル
 class FollowsModel(models.Model):
-    #フォローID
+    # フォローID
     follow_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    #ユーザーID
-    user = models.ForeignKey(Users,on_delete=models.CASCADE,null=True)
-    #ユーザーID（2ついる？ミス？）
-    user = models.ForeignKey(Users,on_delete=models.CASCADE,null=True)
+    # ユーザーID
+    user = models.ForeignKey(Users, on_delete=models.CASCADE, null=True)
+    # ユーザーID（2ついる？ミス？）
+    user = models.ForeignKey(Users, on_delete=models.CASCADE, null=True)
 
-#トークテーブル
+
+# トークテーブル
 class TalksModel(models.Model):
-    #トークID
+    # トークID
     talk_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
-#メッセージテーブル
-class MessagesModel(models.Model):
-    #メッセージID
-    message_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    #メッセージデータ
-    message_data = models.CharField(max_length=100)
-    #メッセージ送信日時
-    massege_date = models.DateTimeField(auto_now_add=True)
-    #イントネーション
-    intnation = models.CharField(max_length=200)
-    #ユーザーID
-    user = models.ForeignKey(Users,on_delete=models.CASCADE,null=True)
-    #トークID
-    talk_id = models.ForeignKey(TalksModel,on_delete=models.CASCADE,null=True)
 
+# メッセージテーブル
+class MessagesModel(models.Model):
+    # メッセージID
+    message_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    # メッセージデータ
+    message_data = models.CharField(max_length=100)
+    # メッセージ送信日時
+    massege_date = models.DateTimeField(auto_now_add=True)
+    # イントネーション
+    intnation = models.CharField(max_length=200)
+    # ユーザーID
+    user = models.ForeignKey(Users, on_delete=models.CASCADE, null=True)
+    # トークID
+    talk_id = models.ForeignKey(TalksModel, on_delete=models.CASCADE, null=True)
--- src/sometaroka/serializers.py
+++ src/sometaroka/serializers.py
@@ -8,3 +8,13 @@ class SampleSerializer(serializers.ModelSerializer):
         model = MessagesModel  # 呼び出すモデル
         # fields = ["message_id", "message_data", "massege_date"]  # API上に表示するモデルのデータ項目
         fields = "__all__"  # 指定せず全ての項目をレスポンスする場合
+
+    def create(self, validated_data):
+        newMessage = MessagesModel.objects.create(
+            message_data=validated_data["message_data"],
+            intnation=validated_data["intnation"],
+            user=validated_data["user"],
+            talk_id=validated_data["talk_id"],
+        )
+        newMessage.save()
+        return newMessage
